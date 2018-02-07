a9e549bb812a3013a3185b29913381a09b112dd0
giuem <giuemcom@gmail.com>
2018-02-07 23:11:23 +0800

    fix monitor click bug

--- public/src/js/open.js
+++ public/src/js/open.js
@@ -1,17 +1,18 @@
 (function() {
-  var monitors = document.querySelectorAll(".monitors.has-children");
+  var monitors = document.querySelectorAll(".monitors.has-children > .monitors-header");
 
   for (var i = 0; i < monitors.length; ++i) {
     monitors[i].addEventListener("click", function(event) {
-      var classList = this.classList;
-      var contentWrap = this.querySelector(".monitors-content-wrap");
+      var parent = this.parentNode;
+      var classList = parent.classList;
+      var contentWrap = parent.querySelector(".monitors-content-wrap");
       if (classList.contains("open")) {
         classList.remove("open");
         contentWrap.style.maxHeight = 0;
       } else {
         classList.add("open");
         contentWrap.style.maxHeight =
-          this.querySelector(".monitors-content").clientHeight + "px";
+        parent.querySelector(".monitors-content").clientHeight + "px";
       }
     });
   }
