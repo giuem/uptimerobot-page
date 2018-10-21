637d0ff24550e597409238e91aeca26bbbea5bc6
lwl12 <i@lwl12.com>
2018-10-21 09:32:04 +0800

    use regex named group for parser

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,6 +1,7 @@
 const patterns = {
-  name: ".+",
-  group: ".+"
+  name: "(?<name>.+)",
+  group: "(?<group>.+)",
+  index: "(?<index>\d+)?"
 };
 /**
  * parse monitor name
@@ -16,28 +17,18 @@ export class Parser {
    */
   constructor(rule) {
     // parse rule
-    this.fields = [];
     for (const pattern in patterns) {
       const index = rule.indexOf(pattern);
       if (index > -1) {
-        this.fields.push({ key: pattern, index });
         rule = rule.replace(`$${pattern}`, `(${patterns[pattern]})`);
       }
     }
-    this.fields.sort(function(a, b) {
-      return a.index - b.index;
-    });
 
     // init regex
     this.regex = new RegExp(rule);
   }
 
   parse(str) {
-    const result = {};
-    const matches = str.match(this.regex);
-    this.fields.map(function({ key }, index) {
-      result[key] = matches[index + 1];
-    });
-    return result;
+    return str.match(this.regex).groups;
   }
 }
