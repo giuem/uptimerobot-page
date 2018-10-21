6f08e7251ad497c63aff4b5bd086f16c1ee8f815
lwl12 <i@lwl12.com>
2018-10-21 16:06:38 +0800

    another case sensitivity issue

--- src/bootstrap/cron.js
+++ src/bootstrap/cron.js
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 import { logger } from "../lib/logger";
 export default ctx => {
   const prefetchMonitors = new CronJob({
-    cronTime: process.env.cronTime || "*/5 * * * *",
+    cronTime: process.env.CRONTIME || "*/5 * * * *",
     onTick: function() {
       ctx.services.uptimerobot
         .prefetchList()
