e93ca455e9dde1ee1017f0b9e99f64e69ee15329
lwl12 <i@lwl12.com>
2018-10-21 17:37:48 +0800

    fix tests

--- src/lib/__tests__/parser.spec.js
+++ src/lib/__tests__/parser.spec.js
@@ -1,54 +1,54 @@
 import test from "ava";
 import { Parser } from "../parser";
 
-test("parse $name/$group/$index", t => {
-  let parser = new Parser("$name/$group/$index");
+test("parse %name/%group/%index", t => {
+  let parser = new Parser("%name/%group/%index");
   let result = parser.parse("aaa/bbb/1");
   t.is(result.name, "aaa");
   t.is(result.group, "bbb");
   t.is(result.index, 1);
 });
 
-test("parse $group/$index/$name", t => {
-  let parser = new Parser("$group/$index/$name");
+test("parse %group/%index/%name", t => {
+  let parser = new Parser("%group/%index/%name");
   let result = parser.parse("aaa/10/bbb");
   t.is(result.name, "bbb");
   t.is(result.group, "aaa");
   t.is(result.index, 10);
 });
 
-test("parse $name/$group/$index when index is omitted", t => {
-  let parser = new Parser("$name/$group/$index");
+test("parse %name/%group/%index when index is omitted", t => {
+  let parser = new Parser("%name/%group/%index");
   let result = parser.parse("aaa/bbb/");
   t.is(result.name, "aaa");
   t.is(result.group, "bbb");
   t.is(result.index, undefined);
 });
 
-test("parse $group/$index/$name when index is omitted", t => {
-  let parser = new Parser("$group/$index/$name");
+test("parse %group/%index/%name when index is omitted", t => {
+  let parser = new Parser("%group/%index/%name");
   let result = parser.parse("aaa//bbb");
   t.is(result.name, "bbb");
   t.is(result.group, "aaa");
   t.is(result.index, undefined);
 });
 
-test("parse $name/$group", t => {
-  let parser = new Parser("$name/$group");
+test("parse %name/%group", t => {
+  let parser = new Parser("%name/%group");
   let result = parser.parse("aaa/bbb");
   t.is(result.name, "aaa");
   t.is(result.group, "bbb");
 });
 
-test("parse $group/$name", t => {
-  const parser = new Parser("$group/$name");
+test("parse %group/%name", t => {
+  const parser = new Parser("%group/%name");
   let result = parser.parse("aaa/bbb");
   t.is(result.name, "bbb");
   t.is(result.group, "aaa");
 });
 
-test("parse $name", t => {
-  const parser = new Parser("$name");
+test("parse %name", t => {
+  const parser = new Parser("%name");
   let result = parser.parse("aaa");
   t.is(result.name, "aaa");
   t.is(result.group, undefined);
